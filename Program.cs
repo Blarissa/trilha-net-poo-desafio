e65ec83ea8e02a92ac5877fde85d7cee496159de
Blarissa <larissabrasil009@gmail.com>
2023-09-24 20:01:37 -0300

    feat: resolução do desafio proposto

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia: ");
+
+var nokia = new Nokia("123456", "11111111", "Modelo 1", 128);
+nokia.InstalarAplicativo("Whatsapp");
+nokia.Ligar();
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone:");
+
+var iphone = new Iphone("654321", "22222222", "Modelo 2", 64);
+iphone.InstalarAplicativo("Telegram");
+iphone.ReceberLigacao();
